e66f5ec85b673d496ae049e399b7ed6459ccee6e
emirmorgan <aemirdnr@gmail.com>
2022-12-10 01:42:26 +0300

    Update: Bookmarks page content

--- pages/bookmarks.tsx
+++ pages/bookmarks.tsx
@@ -1,14 +1,30 @@
 import Head from "next/head";
+import { Icons } from "../assets/Icons";
 import ListContent from "../components/ListContent";
+import { useAppSelector } from "../utils/hooks";
 
 const Bookmarks = () => {
+  const currentUser: any = useAppSelector((state) => state.user.user);
+
   return (
     <>
       <Head>
         <title>Bookmarks / Twitter</title>
       </Head>
       <div className="w-full h-full min-h-screen max-w-[600px] border-x">
-        <span>Bookmarks</span>
+        <div className="flex flex-col">
+          <div className="flex items-center justify-between px-3 pt-2">
+            <div className="flex flex-col">
+              <span className="font-bold text-xl">Bookmarks</span>
+              <span className="text-gray-500 text-sm">
+                {"@" + currentUser?.username}
+              </span>
+            </div>
+            <div className="ml-auto cursor-pointer">
+              <Icons name="burgerMore" />
+            </div>
+          </div>
+        </div>
       </div>
       <div className="hidden lg:flex flex-col w-[350px] mr-[10px]">
         <ListContent />
