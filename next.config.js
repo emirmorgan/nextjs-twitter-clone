eebc8b461d1637f6329fe9e8bd3018ea6537d3d7
emirmorgan <aemirdnr@gmail.com>
2022-12-14 05:27:46 +0300

    Update: next.config.js env set

--- next.config.js
+++ next.config.js
@@ -10,6 +10,14 @@ const nextConfig = {
       },
     ],
   },
+  env: {
+    API_KEY: process.env.API_KEY,
+    AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+    PROJECT_ID: process.env.PROJECT_ID,
+    STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+    MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
+    APP_ID: process.env.APP_ID,
+  },
 };
 
 module.exports = nextConfig;
